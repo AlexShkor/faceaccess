42825dd1126a1c5e6b5ea76e9606800ca4f6fc0e
Alex Shkor <alex.shkor@gmail.com>
2017-03-27 18:35:18 +0300

    changes?

---
+++ client/views/userProfile/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+     <div>Пользователь {{ $route.params.id }}</div>
+  </div>
+</template>
+<script>
+    export default {
+      data() {
+        return {
+          users: [{
+            Name: "First",
+            Id: "1"
+          }, {
+            Name: "Test",
+            Id: "2"
+          }]
+        }
+      },
+      methods: {
+        view() {
+
+        }
+      }
+    }
+
+
+</script>
---
+++ client/views/users/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+     <h4 class="title">Users</h4>
+     <table class="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Instrument</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in users">
+                <td>
+                
+                  <router-link :to="{ name: 'User', params: { id: user.Id }}">{{user.Name}}</router-link>
+                </td>
+                <td>Bass Guitar</td>
+                <td>Bass Guitar</td>
+                <td >
+                </td>
+              </tr>
+              </tbody>
+      </table>
+  </div>
+</template>
+<script>
+    export default {
+  data () {
+    return {
+      users: [{Name: "First", Id: "1"}, {Name: "Test", Id: "2"}]
+    }
+  },
+  methods: {
+    view () {
+
+    }
+  }
+}
+</script>
